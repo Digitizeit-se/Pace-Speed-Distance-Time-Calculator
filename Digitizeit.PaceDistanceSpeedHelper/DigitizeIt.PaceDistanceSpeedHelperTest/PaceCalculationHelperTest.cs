47d8c08f3ea0e825b2170623959dd9c41956fb1e
John Larsen <john@pcis.se>
2020-06-07 22:31:17 +0200

    Added MPH and decimal

--- Digitizeit.PaceDistanceSpeedHelper/DigitizeIt.PaceDistanceSpeedHelperTest/PaceCalculationHelperTest.cs
+++ Digitizeit.PaceDistanceSpeedHelper/DigitizeIt.PaceDistanceSpeedHelperTest/PaceCalculationHelperTest.cs
@@ -86,6 +86,22 @@ namespace DigitizeIt.PaceDistanceSpeedHelperTest
         }
 
         [Fact]
+        public void Given_a_decimal_distance_in_meter_over_time_in_seconds_the_right_min_km_value_is_returned()
+        {
+            //Arrange
+            decimal distance = 1000;
+            var seconds = 360;
+            double expected = 6;
+
+            //Act
+            var result = distance.DistanceMetersInSecondsToMinKm(seconds);
+
+            //Assert
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
         public void Given_a_float_distance_in_meter_over_time_in_seconds_the_right_minute_over_mile_value_is_returned()
         {
             //Arrange
@@ -166,10 +182,106 @@ namespace DigitizeIt.PaceDistanceSpeedHelperTest
         }
 
         [Fact]
+        public void Given_a_decimal_distance_in_meter_over_time_in_seconds_the_right_minute_over_mile_value_is_returned()
+        {
+            //Arrange
+            decimal distance = 1609;
+            var seconds = 360;
+            double expected = 6;
+
+            //Act
+            var result = distance.DistanceMetersInSecondsToMinutesEnglishMile(seconds);
+
+            //Assert
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
         public void Given_a_int_distance_in_meter_over_time_in_seconds_the_right_Km_hour_value_is_returned()
         {
             //Arrange
             int distance = 1000;
+            var seconds = 360;
+            double expected = 10;
+
+            //Act
+            var result = distance.DistanceMetersInSecondsToKmHour(seconds);
+
+            //Assert
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Given_a_double_distance_in_meter_over_time_in_seconds_the_right_Km_hour_value_is_returned()
+        {
+            //Arrange
+            double distance = 1000;
+            var seconds = 360;
+            double expected = 10;
+
+            //Act
+            var result = distance.DistanceMetersInSecondsToKmHour(seconds);
+
+            //Assert
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Given_a_float_distance_in_meter_over_time_in_seconds_the_right_Km_hour_value_is_returned()
+        {
+            //Arrange
+            float distance = 1000;
+            var seconds = 360;
+            double expected = 10;
+
+            //Act
+            var result = distance.DistanceMetersInSecondsToKmHour(seconds);
+
+            //Assert
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Given_a_short_distance_in_meter_over_time_in_seconds_the_right_Km_hour_value_is_returned()
+        {
+            //Arrange
+            short distance = 1000;
+            var seconds = 360;
+            double expected = 10;
+
+            //Act
+            var result = distance.DistanceMetersInSecondsToKmHour(seconds);
+
+            //Assert
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Given_a_long_distance_in_meter_over_time_in_seconds_the_right_Km_hour_value_is_returned()
+        {
+            //Arrange
+            long distance = 1000;
+            var seconds = 360;
+            double expected = 10;
+
+            //Act
+            var result = distance.DistanceMetersInSecondsToKmHour(seconds);
+
+            //Assert
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Given_a_decimal_distance_in_meter_over_time_in_seconds_the_right_Km_hour_value_is_returned()
+        {
+            //Arrange
+            decimal distance = 1000;
             var seconds = 360;
             double expected = 10;
 
